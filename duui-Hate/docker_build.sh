f34e61c1a7f6736bab7ae7cd06c4e91966a3044e
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2024-06-04 12:27:27 +0200

    DUUI Hate

---
+++ duui-Hate/docker_build.sh
@@ -0,0 +1,145 @@
+export ANNOTATOR_NAME=duui-hate
+export ANNOTATOR_VERSION=0.1.0
+export LOG_LEVEL=INFO
+eport MODEL_CACHE_SIZE=3
+
+#---------------------------------------------------------------------
+#export  MODEL_NAME="Andrazp/multilingual-hate-speech-robacofi"
+#export MODEL_SPECNAME="andrazp"
+#export MODEL_VERSION="c2b98c47f5e13c326a7af48ba544fff4d93fbc70"
+#export MODEL_SOURCE="https://huggingface.co/Andrazp/multilingual-hate-speech-robacofi"
+#export MODEL_LANG="Multi"
+#--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="alexandrainst/da-hatespeech-detection-base"
+#export MODEL_SPECNAME="alexandrainst"
+#export MODEL_VERSION="6ec0fe1587f6038765b0d7f59525dd4162c4acb2"
+#export MODEL_SOURCE="https://huggingface.co/alexandrainst/da-hatespeech-detection-base"
+#export MODEL_LANG="Multi"
+##--------------------------------------------------------------------
+#
+##---------------------------------------------------------------------
+#export  MODEL_NAME="l3cube-pune/me-hate-roberta"
+#export MODEL_SPECNAME="l3cube"
+#export MODEL_VERSION="63890c746c153af20a6cd9832ccbeda03e0d960b"
+#export MODEL_SOURCE="https://huggingface.co/l3cube-pune/me-hate-roberta"
+#export MODEL_LANG="Multi"
+##--------------------------------------------------------------------
+#
+##---------------------------------------------------------------------
+#export  MODEL_NAME="GroNLP/hateBERT"
+#export MODEL_SPECNAME="gronlp"
+#export MODEL_VERSION="1d439ddf8a588fc8c44c4169ff9e102f3e839cca"
+#export MODEL_SOURCE="https://huggingface.co/GroNLP/hateBERT"
+#export MODEL_LANG="EN"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="cardiffnlp/twitter-roberta-base-hate-latest"
+#export MODEL_SPECNAME="cardiffnlp"
+#export MODEL_VERSION="c74b0534df96af8232f6a3ffdb90d9a72223d7b7"
+#export MODEL_SOURCE="https://huggingface.co/cardiffnlp/twitter-roberta-base-hate-latest"
+#export MODEL_LANG="EN"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-english"
+#export MODEL_SPECNAME="cnerg"
+#export MODEL_VERSION="25d0e4d9122d2a5c283e07405a325e3dfd4a73b3"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-english"
+#export MODEL_LANG="EN"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="deepset/bert-base-german-cased-hatespeech-GermEval18Coarse"
+#export MODEL_SPECNAME="deepset"
+#export MODEL_VERSION="70e4821931a8a685d83bc0e8bd8877157bdb3883"
+#export MODEL_SOURCE="https://huggingface.co/deepset/bert-base-german-cased-hatespeech-GermEval18Coarse"
+#export MODEL_LANG="DE"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-german"
+#export MODEL_SPECNAME="cnergde"
+#export MODEL_VERSION="53a24df030e8e20e7880a161494fb5922ce34617"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-german"
+#export MODEL_LANG="DE"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-spanish"
+#export MODEL_SPECNAME="cnerges"
+#export MODEL_VERSION="2b9664ac59ee7f0b054fc0b1433cbedff3c2bdba"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-spanish"
+#export MODEL_LANG="ES"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-polish"
+#export MODEL_SPECNAME="cnergpl"
+#export MODEL_VERSION="ec586b2e2e6140879c6f533ccd5208d1c2692715"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-polish"
+#export MODEL_LANG="PL"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-portugese"
+#export MODEL_SPECNAME="cnergpt"
+#export MODEL_VERSION="a212b2dd7e8e3d953787a49d92c469b30c6da6ba"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-portugese"
+#export MODEL_LANG="PT"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-italian"
+#export MODEL_SPECNAME="cnergit"
+#export MODEL_VERSION="aeb70b454d5fc3046aa2a062c525d1ac60f2f01b"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-italian"
+#export MODEL_LANG="IT"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-arabic"
+#export MODEL_SPECNAME="cnergar"
+#export MODEL_VERSION="e592a5ee3b913ec33286ee90fb27c7f7f1a8b996"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-arabic"
+#export MODEL_LANG="AR"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+#export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-french"
+#export MODEL_SPECNAME="cnergfr"
+#export MODEL_VERSION="7c0e8c45e9176581e57d4ae7e52327258116f969"
+#export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-french"
+#export MODEL_LANG="FR"
+##--------------------------------------------------------------------
+
+##---------------------------------------------------------------------
+export  MODEL_NAME="Hate-speech-CNERG/dehatebert-mono-indonesian"
+export MODEL_SPECNAME="cnergid"
+export MODEL_VERSION="08693d6cc64f7e7b3019b2a3abe3b1a9c8ca74c2"
+export MODEL_SOURCE="https://huggingface.co/Hate-speech-CNERG/dehatebert-mono-indonesian"
+export MODEL_LANG="ID"
+##--------------------------------------------------------------------
+
+export DOCKER_REGISTRY="docker.texttechnologylab.org/"
+export DUUI_CUDA=
+#export DUUI_CUDA="-cuda"
+
+docker build \
+  --build-arg ANNOTATOR_NAME \
+  --build-arg ANNOTATOR_VERSION \
+  --build-arg LOG_LEVEL \
+  --build-arg MODEL_CACHE_SIZE \
+  --build-arg MODEL_NAME \
+  --build-arg MODEL_VERSION \
+  --build-arg MODEL_SOURCE \
+  --build-arg MODEL_LANG \
+  -t ${DOCKER_REGISTRY}${ANNOTATOR_NAME}"-"${MODEL_SPECNAME}:${ANNOTATOR_VERSION}${DUUI_CUDA} \
+  -f src/main/docker/Dockerfile${DUUI_CUDA} \
+  .
+
+docker tag \
+  ${DOCKER_REGISTRY}${ANNOTATOR_NAME}"-"${MODEL_SPECNAME}:${ANNOTATOR_VERSION}${DUUI_CUDA} \
+  ${DOCKER_REGISTRY}${ANNOTATOR_NAME}"-"${MODEL_SPECNAME}:latest${DUUI_CUDA}
