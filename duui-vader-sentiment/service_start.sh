d20dd64a65e274ea4d5d40822d1dfb8398860e11
Daniel Baumartz <baumartz@stud.uni-frankfurt.de>
2024-08-22 10:46:43 +0200

    moved gervader to vader tool

--- duui-vader-sentiment/service_start.sh
+++ duui-vader-sentiment/service_start.sh
@@ -1,4 +1,4 @@
-TEXTIMAGER_DUUI_VADER_SENTIMENT_ANNOTATOR_NAME="textimager-duui-vader-sentiment" \
+TEXTIMAGER_DUUI_VADER_SENTIMENT_ANNOTATOR_NAME="duui-vader-sentiment" \
 TEXTIMAGER_DUUI_VADER_SENTIMENT_ANNOTATOR_VERSION="unset" \
 TEXTIMAGER_DUUI_VADER_SENTIMENT_LOG_LEVEL="DEBUG" \
 uvicorn src.main.python.textimager_duui_vader_sentiment:app --host 0.0.0.0 --port 9714 --workers 1
