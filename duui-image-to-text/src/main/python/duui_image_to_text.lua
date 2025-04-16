661812260aa7e02c90e051e66ef3bf05d4c257c8
Ali Abusaleh <alighabusaleh@gmail.com>
2025-04-16 12:34:47 +0200

    support results with no entities, and add prompts to annotation

--- duui-image-to-text/src/main/python/duui_image_to_text.lua
+++ duui-image-to-text/src/main/python/duui_image_to_text.lua
@@ -4,17 +4,17 @@ JCasUtil = luajava.bindClass("org.apache.uima.fit.util.JCasUtil")
 TopicUtils = luajava.bindClass("org.texttechnologylab.DockerUnifiedUIMAInterface.lua.DUUILuaUtils")
 
 function serialize(inputCas, outputStream, parameters)
-     print("start Lua serialzation")
+     --print("start Lua serialzation")
     local doc_lang = inputCas:getDocumentLanguage()
 
-    print("doc_lang: ", doc_lang)
+    --print("doc_lang: ", doc_lang)
 
     -- get the parameters promopt or default
     local prompt = parameters["prompt"] if parameters["prompt"] == nil then prompt = "<grounding>An image of" end
     local model_name = parameters["model_name"] if parameters["model_name"] == nil then model_name = "microsoft/kosmos-2-patch14-224" end
     local selection_types = parameters["selections"] if parameters["selections"] == nil then selection_types="org.texttechnologylab.annotation.type.Image" end
     --print("truncate_text: ", truncate_text)
-    print("start")
+    --print("start")
     local images = {}
     local number_of_images = 1
     for selection_type in string.gmatch(selection_types, "([^,]+)") do
@@ -65,6 +65,14 @@ function deserialize(inputCas, inputStream)
     --print("results")
     --print(results)
 
+    if results['prompt'] ~= nil then
+        local prompt = results['prompt']
+        local prompt_i = luajava.newInstance("org.texttechnologylab.annotation.AnnotationComment", inputCas)
+        prompt_i:setKey("prompt")
+        prompt_i:setValue(prompt)
+        prompt_i:addToIndexes()
+    end
+
     if results['errors'] ~= nil then
         local errors = results['errors']
         for index_i, error in ipairs(errors) do
@@ -74,16 +82,16 @@ function deserialize(inputCas, inputStream)
             warning_i:addToIndexes()
         end
     end
-    print("---------------------- Finished errors ----------------------")
+    --print("---------------------- Finished errors ----------------------")
 
     if results['model_source'] ~= nil and results['model_version'] ~= nil and results['model_name'] ~= nil and results['model_lang'] ~= nil then
-         print("GetInfo")
+         --print("GetInfo")
         local source = results["model_source"]
         local model_version = results["model_version"]
         local model_name = results["model_name"]
         local model_lang = results["model_lang"]
 
-         print("setMetaData")
+         --print("setMetaData")
         local model_meta = luajava.newInstance("org.texttechnologylab.annotation.model.MetaData", inputCas)
         model_meta:setModelVersion(model_version)
 --         print(model_version)
@@ -123,6 +131,7 @@ function deserialize(inputCas, inputStream)
 
                 -- create a subimage using entities
                 for entity_name, entity_data in pairs(results_entities) do
+
                     --print("entity_name: ", entity_name)
                     local subimage_i = luajava.newInstance("org.texttechnologylab.annotation.type.SubImage", inputCas)
                     --print("entity_name: ", entity_name, "type: ", type(entity_name))
@@ -133,28 +142,37 @@ function deserialize(inputCas, inputStream)
                     --print(entity_data["end"])
                     --print(entity_data["bounding_box"])
                     --print("starting bboxes")
-                    local coordinates = luajava.newInstance("org.apache.uima.jcas.cas.FSArray", inputCas, 2)
-                    subimage_i:setCoordinates(coordinates)
-                    local idx = 0
-                    for bx1, bx2 in pairs(entity_data["bounding_box"]) do
-                        --print(("idx: %d"):format(idx))
-                        --print("x1: ", bx2[1])
-                        --print("y1:", bx2[2])
-                        --
-                        --print("x2: ", bx2[3])
-                        --print("y2: ", bx2[4])
-                        local coordinate_i = luajava.newInstance("org.texttechnologylab.annotation.type.Coordinate", inputCas)
-                        coordinate_i:setX(bx2[1])
-                        coordinate_i:setY(bx2[2])
-                        coordinate_i:addToIndexes()
-                        subimage_i:setCoordinates(idx, coordinate_i)
+                    print("len of bounding_box: ", #entity_data["bounding_box"])
+                    local coordinates = luajava.newInstance("org.apache.uima.jcas.cas.FSArray", inputCas, #entity_data["bounding_box"])
+                    if #entity_data["bounding_box"] > 0 then
+                        subimage_i:setCoordinates(coordinates)
+                        local idx = 0
+                        for bx1, bx2 in pairs(entity_data["bounding_box"]) do
+                            --print(("idx: %d"):format(idx))
+                            --print("x1: ", bx2[1])
+                            --print("y1:", bx2[2])
+                            --
+                            --print("x2: ", bx2[3])
+                            --print("y2: ", bx2[4])
+                            if idx < #entity_data["bounding_box"] then
+                                local coordinate_i = luajava.newInstance("org.texttechnologylab.annotation.type.Coordinate", inputCas)
+                                coordinate_i:setX(bx2[1])
+                                coordinate_i:setY(bx2[2])
+                                coordinate_i:addToIndexes()
+                                subimage_i:setCoordinates(idx, coordinate_i)
 
-                        local coordinate_i1 = luajava.newInstance("org.texttechnologylab.annotation.type.Coordinate", inputCas)
-                        coordinate_i1:setX(bx2[3])
-                        coordinate_i1:setY(bx2[4])
-                        coordinate_i1:addToIndexes()
-                        subimage_i:setCoordinates(idx + 1, coordinate_i1)
-                        idx = idx + 2
+                            end
+
+                            if (idx + 1) < #entity_data["bounding_box"] then
+                                local coordinate_i1 = luajava.newInstance("org.texttechnologylab.annotation.type.Coordinate", inputCas)
+                                coordinate_i1:setX(bx2[3])
+                                coordinate_i1:setY(bx2[4])
+                                coordinate_i1:addToIndexes()
+                                subimage_i:setCoordinates(idx + 1, coordinate_i1)
+                                idx = idx + 2
+
+                            end
+                        end
 
                     end
                     --subimage_i:setCoordinates(coordinates)
