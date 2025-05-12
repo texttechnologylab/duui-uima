f61c5af2423698888a3a62aadfe1253b52326dfb
Ali Abusaleh <alighabusaleh@gmail.com>
2025-05-12 13:11:22 +0200

    update servers docker files

--- duui-vision-language/src/main/python/start_vllm_server.sh
+++ duui-vision-language/src/main/python/start_vllm_server.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 set -e
 
+export VLLM_PLATFORM=cuda
+
+pip uninstall libtpu-nightly jax jaxlib -y
+
 
 echo "Launching vLLM server..."
 # Load and run the model:
-vllm serve "allenai/Molmo-72B-0924" --trust-remote-code
+vllm serve "allenai/Molmo-7B-D-0924" \
+ --trust-remote-code --device auto \
+ --revision ac032b93b84a7f10c9578ec59f9f20ee9a8990a2 \
+  --tensor-parallel-size 1 \
+  --pipeline-parallel-size 1 \
+  --distributed-executor-backend mp \
+  --dtype auto \
+  --trust-remote-code \
+  &
 
 VLLM_PID=$!
 
 echo "Waiting for vLLM to be ready..."
 until curl -s http://localhost:8000/v1/models > /dev/null; do
   echo "Still waiting for vLLM..."
-  sleep 2
+  sleep 5
 done
 
 echo "Launching FastAPI server..."
